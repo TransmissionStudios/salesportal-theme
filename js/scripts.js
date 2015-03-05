6649471558d49bda4244db0236a55e8ac45728ff
Richard McMenamin <richard@transmissionstudios.com>
2015-03-05 14:51:14 +0000

    Added intial css using Susy for case study CT and js for naver

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+(function($) {
+  'use strict';
+
+  $(document).ready(function() {
+
+    $('nav ul.menu').naver({
+      labels: {
+        closed: '',
+        open: ''
+      },
+      maxWidth: '768px'
+    });
+
+  });
+})(jQuery);
